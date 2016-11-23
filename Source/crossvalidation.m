51dc214bbd0c05176ebf4c916e420ac27d4aef44
Daniel Gehrig <danielgehrig@daniels-macbook-pro-3.home>
2016-11-23 23:30:09 +0100

    Fallunterscheidung für bagging

--- Source/crossvalidation.m
+++ Source/crossvalidation.m
@@ -4,6 +4,7 @@ function [ testvals ] = crossvalidation(CMP,Xtrain,ytrain,Wtrain,Xtest,ytest,Wte
 global s b i
 load_path = ['models/model_' num2str(s) '_' num2str(b) '.mat'];
 
+
 m = load(load_path);
 m = m.model;
 % template tree parameters
@@ -11,18 +12,20 @@ tree = m.ModelParameters.LearnerTemplates{1};
 
 % get parameters 
 n_learn = m.ModelParameters.NLearn;
-learning_rate = m.ModelParameters.LearnRate;
 method = m.ModelParameters.Method;
-m = fitcensemble(Xtrain, ytrain, 'Method', method, 'NumLearningCycles', n_learn, 'LearnRate', learning_rate, 'Learners', tree); 
+if strcmp(method, 'Bagging')
+    learning_rate = m.ModelParameters.LearnRate;
+    m = fitcensemble(Xtrain, ytrain, 'Method', method, 'NumLearningCycles', n_learn, 'LearnRate', learning_rate, 'Learners', tree); 
+else
+    m = fitcensemble(Xtrain, ytrain, 'Method', method, 'NumLearningCycles', n_learn, 'Learners', tree); 
+end
 
 [~,scores] = predict(m, Xtest);
 
 yfit = scores(:,2);
 
-if ~isempty(find(yfit < 0 | yfit > 1))
-    i = load('i.mat');
-    i.i
-    yfit = 1 ./ (1 + exp(-i.i*yfit));
+if ~strcmp(method, 'Bagging')
+    yfit = 1 ./ (1 + exp(-i*yfit));
 end
 
 testvals = -mean(ytest .* log(yfit) + (1 - ytest) .* log(1 - yfit));
