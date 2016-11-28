fe2a654616bbf29f0c976ead123ffa936586007e
Adrian <rucklia@student.ethz.ch>
2016-11-28 18:44:56 +0100

    Changes for optimization reasons

--- cv_test.m
+++ cv_test.m
@@ -17,28 +17,74 @@ end
 y = csvread('targets.csv');
 
 %%
-for s = 2:10    
-    for b = 2:5
-        X_path = ['Xs/X_' num2str(s) '_' num2str(b) '.mat'];
-        x_struct = load(X_path);
-        X = x_struct.X;
-        
-        model = fitcensemble(X,y,'OptimizeHyperparameters','auto',...
+SaveM = cell(3,1);
+bvec = 6:8;
+disp('Evaluation started')
+tic
+% for s = 2:10    
+%     parfor c = 1:3
+%         b = bvec(c);
+%         X_path = ['Xs/X_' num2str(s) '_' num2str(b) '.mat'];
+%         x_struct = load(X_path);
+%         X = x_struct.X;
+        c = 1;
+        SaveM{c} = fitcensemble(X,y,'OptimizeHyperparameters','auto',...
             'HyperparameterOptimizationOptions',struct('AcquisitionFunctionName',...
             'expected-improvement-plus', 'MaxObjectiveEvaluations', 30, 'SaveIntermediateResults', 0, 'Verbose', 1, 'ShowPlots', 1, 'Kfold', 20));
 
-        save(model, ['models/model_' num2str(s) '_' num2str(b) '.mat']);
+        
+%     end
+%     disp(['Evaluated ' num2str(s)])
+%     for c = 1:3
+%         b = bvec(c);
+        model = SaveM{c};
+%         save(['models/model_' num2str(s) '_' num2str(b) '.mat'], 'model');
+%     end
+%     disp(['Saved ' num2str(s)])
+% end
+disp('Evaluation completed')
+t = toc;
+
+%%
+global globmodel iparameter
+SaveCVmean = cell(10,1);
+SaveCVstd = cell(10,1);
+SaveCVstrange = cell(10,1);
+irange = 0.2:0.2:2;
+brange = 6:8;
+srange = 2:9;
+tottime = 0;
+disp('Cross validiation started ...')
+for i= 1:length(irange)
+    iparameter = irange(i);
+    CVmeanmat = zeros(8,3);
+    CVstrangemat = CVmeanmat;
+    CVstdmat = CVmeanmat;
+for is = 1:length(srange)
+    s = srange(is);
+    for ib = 1:length(brange)
+        tic
+        b = brange(ib);
+        load(['models/model_' num2str(s) '_' num2str(b) '.mat']) 
+        globmodel = model;
+        cv_model = crossval(model); % TODO: way to pass model function and parameters          
+%     save('i.mat', 'i');
+            CV = kfoldfun(cv_model, @crossvalidation);
+            tempmean = mean(CV);
+            CVmeanmat(is,ib) = tempmean;
+            CVstrangemat(is,ib) = mean((CV-tempmean(end)).^2).^5;
+            CVstdmat(is,ib) = std(CV);
+            
+%             s = [s, mean((CV-m(end)).^2)^.5];
+            t = toc;
+            tottime = tottime + t;
+            disp(['Cross validation of ' num2str(s) ' chunks and ' num2str(b) ' bins and i parameter ' num2str(iparameter) ' done in ' num2str(t) ' seconds.'])
+            disp(['CV mean ' num2str(CVmeanmat(is,ib)) ', CV deviation ' num2str(CVstdmat(is,ib)) ' Total elapsed time ' num2str(tottime)])
     end
 end
-%%
-cv_model = crossval(model); % TODO: way to pass model function and parameters
-is = .1:.1:1;
-ms = [];
-ss = [];
-for i=.1:.1:1
-    save('i.mat', 'i');
-    i
-    CV = kfoldfun(cv_model, @crossvalidation);
-    m = [m, mean(CV)];
-    s = [s, mean((CV-m(end)).^2)^.5];
+    SaveCVmean{i} = CVmeanmat;
+    SaveCVstrange{i} = CVstrangemat;
+    SaveCVstd{i} = CVstdmat;
 end
+disp('Cross validation completed')
+clearvars globmodel iparameter
