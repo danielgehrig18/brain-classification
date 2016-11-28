38f307d50239d383fed239115f3821390f55550e
Adrian <rucklia@student.ethz.ch>
2016-11-28 18:44:37 +0100

    Changes to make work for optimization

--- Source/crossvalidation.m
+++ Source/crossvalidation.m
@@ -2,25 +2,33 @@ function [ testvals ] = crossvalidation(CMP,Xtrain,ytrain,Wtrain,Xtest,ytest,Wte
 %CROSSVALIDATION Summary of this function goes here
 %   Detailed explanation goes here
 
-m = load('models/model.mat');
-m = m.model;
+% m = load('models/model.mat');
+% m = m.model;
 % template tree parameters
-tree = m.ModelParameters.LearnerTemplates{1};
+global globmodel iparameter
+tree = globmodel.ModelParameters.LearnerTemplates{1};
 
 % get parameters 
-n_learn = m.ModelParameters.NLearn;
-learning_rate = m.ModelParameters.LearnRate;
-method = m.ModelParameters.Method;
-m = fitcensemble(Xtrain, ytrain, 'Method', method, 'NumLearningCycles', n_learn, 'LearnRate', learning_rate, 'Learners', tree); 
+n_learn = globmodel.ModelParameters.NLearn;
+learning_rate = globmodel.ModelParameters.LearnRate;
+method = globmodel.ModelParameters.Method;
+if strcmp(method,'Bag')
+    % LearnRate is not an option of bagging
+    m = fitcensemble(Xtrain, ytrain, 'Method', method, 'NumLearningCycles', n_learn, 'Learners', tree);
+else
+    m = fitcensemble(Xtrain, ytrain, 'Method', method, 'NumLearningCycles', n_learn, 'LearnRate', learning_rate, 'Learners', tree); 
+end
 
 [~,scores] = predict(m, Xtest);
 
 yfit = scores(:,2);
 
-if ~isempty(find(yfit < 0 | yfit > 1))
-    i = load('i.mat');
-    i.i
-    yfit = 1 ./ (1 + exp(-i.i*yfit));
+idxTrans = (yfit > 0 & yfit < 1);
+
+if sum(idxTrans) < length(idxTrans) 
+%     i = load('i.mat');
+%     i.i
+    yfit = 1 ./ (1 + exp(-iparameter*yfit));
 end
 
 testvals = -mean(ytest .* log(yfit) + (1 - ytest) .* log(1 - yfit));
